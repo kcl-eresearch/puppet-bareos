7bc91f543e9d3c527e37889d5abbbe6ddf14f0a8
Xand Meaden <xand.meaden@kcl.ac.uk>
2025-06-04 17:23:42 +0100

    chore: remove unhelpful default limits

--- manifests/profile/director/pool.pp
+++ manifests/profile/director/pool.pp
@@ -2,33 +2,27 @@
 # Some default pools
 class bareos::profile::director::pool {
   bareos::director::pool { 'Differential':
-    pool_type            => 'Backup',
-    recycle              => true,
-    auto_prune           => true,
-    volume_retention     => '90 days',
-    maximum_volume_bytes => '10G',
-    maximum_volumes      => 100,
-    label_format         => 'Differential-',
+    pool_type        => 'Backup',
+    recycle          => true,
+    auto_prune       => true,
+    volume_retention => '90 days',
+    label_format     => 'Differential-',
   }
 
   bareos::director::pool { 'Full':
-    pool_type            => 'Backup',
-    recycle              => true,
-    auto_prune           => true,
-    volume_retention     => '365 days',
-    maximum_volume_bytes => '50G',
-    maximum_volumes      => 100,
-    label_format         => 'Full-',
+    pool_type        => 'Backup',
+    recycle          => true,
+    auto_prune       => true,
+    volume_retention => '365 days',
+    label_format     => 'Full-',
   }
 
   bareos::director::pool { 'Incremental':
-    pool_type            => 'Backup',
-    recycle              => true,
-    auto_prune           => true,
-    volume_retention     => '30 days',
-    maximum_volume_bytes => '1G',
-    maximum_volumes      => 100,
-    label_format         => 'Incremental-',
+    pool_type        => 'Backup',
+    recycle          => true,
+    auto_prune       => true,
+    volume_retention => '30 days',
+    label_format     => 'Incremental-',
   }
 
   bareos::director::pool { 'Scratch':
